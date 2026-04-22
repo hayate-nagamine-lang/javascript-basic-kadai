f3f576f844acda6354a5577d125db8cb00e0847e
hayate-nagamine-lang <hayate-nagamine@gr.paramix.co.jp>
2026-04-22 14:38:31 +0900

    Add files via upload

---
+++ kadai_020/event.js
@@ -0,0 +1,8 @@
+const textElement = document.getElementById('text');
+
+const btnElement = document.getElementById('btn');
+
+btnElement.addEventListener('click' , () =>  {
+  
+  textElement.textContent = 'ボタンをクリックしました';
+});
